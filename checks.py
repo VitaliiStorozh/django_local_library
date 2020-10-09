2f2123dc74958b72f668a91915c8db6002d34b9c
Vitalii <sv301185@gmail.com>
2020-10-09 13:06:14 +0300

    fatal error

--- checks.py
+++
@@ -1,9 +0,0 @@
-from authentication.models import CustomUser
-
-
-def check_is_authenticated(request):
-    return request.user.is_authenticated
-
-
-def check_is_admin(request):
-    return CustomUser.get_by_email(request.user.email).role == 1
